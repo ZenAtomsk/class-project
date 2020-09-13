1b3b1649468be4253087d93fc2f6c16a30f90083
ZenAtomsk <ricardo.b3788@gmail.com>
2020-09-12 20:44:00 -0700

    practicing stuff

---
+++ js/how-much.js
@@ -0,0 +1,15 @@
+'use strict'
+
+var response = prompt('How much wood, could a woodchuck chuck, if a woodchuck could chuck wood?');
+var message;
+
+if (response === '0' ) {
+    message = 'Well that is not a fun guess!';
+} else if (response === '7') {
+    message = 'How did you know my lucky number?';
+} else {
+    message = 'The world may never know';
+}
+
+
+document.write('<h1>' + message )
